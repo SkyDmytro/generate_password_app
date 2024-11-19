fa99db5bd1c11114357426e8f8e2b62fef1f6b5c
Dima <skydmytro@gmail.com>
2024-11-19 18:27:32 +0100

    test commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`$ ${geistMono.variable} antialiased`}>{children}</body>
+      <body className={`$${geistMono.variable} antialiased`}>{children}</body>
     </html>
   );
 }
