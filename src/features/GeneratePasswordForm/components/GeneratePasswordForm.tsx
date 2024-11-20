4919835c3f6cca31f01018ac79eaabe7884290fa
Dima <skydmytro@gmail.com>
2024-11-20 14:04:21 +0100

    build: eslint config change

--- src/features/GeneratePasswordForm/components/GeneratePasswordForm.tsx
+++ src/features/GeneratePasswordForm/components/GeneratePasswordForm.tsx
@@ -25,7 +25,6 @@ export const GeneratePasswordForm = () => {
 
   useEffect(() => {
     setLength(sliderPercent);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [sliderPercent]);
 
   useEffect(() => {
